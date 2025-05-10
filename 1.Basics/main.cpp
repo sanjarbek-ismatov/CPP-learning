24a84f93e120d74d57157ec1231f5f4c9fab503a
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2025-05-10 21:50:35 +0500

    basic code for comparison

--- 1.Basics/main.cpp
+++ 1.Basics/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 int main(){
-    int a = 5;
-    std::cout << "Hello World!" << std::endl;
+    int* p = new int[10];
+    for (int i = 0; i < 10; i++){
+        p[i] = i;
+    }
+    for (int j = 0; j < 10; j++){
+        std::cout << p[j] << std::endl;
+    }
     return 0;
 }
